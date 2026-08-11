109f96ce7feb5e9f50922ea8c177e5380cf50239
karthiksaivinjamarla-jpg <karthiksaivinjamarla@gmail.com>
2026-08-12 00:02:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@
 const CONFIG = {
     API_URL: "https://script.google.com/macros/s/AKfycbz_1QzFyPz82upcA8OYMuBLy36iMaKhKd5kFfAIPf6FlcTnbCBiFs2lDg6IdCPBCSVqfQ/exec",
 
-    QR_IMAGE_URL =
-    "https://raw.githubusercontent.com/karthiksaivinjamarla-jpg/Codex-4.0/main/codex-payment-qr.png";
+    QR_IMAGE_URL: "https://raw.githubusercontent.com/karthiksaivinjamarla-jpg/Codex-4.0/main/codex-payment-qr.png",
 
     EVENT_NAME: "CODEX 4.0",
     ORGANIZER: "Coders' Club",
